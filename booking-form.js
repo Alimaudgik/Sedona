27210e46d1ff3266684622d0beccab62c27a7cda
имя_пользователя_github <arseni_p@tut.by>
2019-01-31 16:58:52 +0300

    js

--- booking-form.js
+++ booking-form.js
@@ -1,18 +1,37 @@
 var bookingForm = document.querySelector(".search-form");
 var searchButton = document.querySelector(".search-button");
+var Popup = bookingForm.querySelector(".modal-show");
 
-    searchButton.addEventListener("click", function (evt) {
+searchButton.addEventListener("click", function (evt) {
     evt.preventDefault();
     bookingForm.classList.add("modal-show");
+
 });
+/* 
+if (Popup.classList.contains("modal-show")) {
+    searchButton.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        Popup.classList.remove("modal-show");   
+    });
+});
+
+   if (bookingForm.classList.contains("modal-show")) {
+        bookingForm.classList.remove("modal-show");
+    } */
 
 
 window.addEventListener("keydown", function (evt) {
     evt.preventDefault();
     if (evt.keyCode === 27) {
-        if (bookingForm.classList.contains("modal-show")) {
+        if (Popup) {
             bookingForm.classList.remove("modal-show");
         }
     }
 }
-)
+);
+
+
+
+
+
+
